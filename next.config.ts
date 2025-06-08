5f4d8c58c62d88da8042b65f5837bc64ed0a8525
David Maldonado Pizarro <jose.maldonado.p@tecsup.edu.pe>
2025-06-07 21:17:50 -0500

    Configurar next.config.ts para ignorar ESLint en build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // aquí puedes agregar más configuraciones si las tienes
 };
 
 export default nextConfig;
